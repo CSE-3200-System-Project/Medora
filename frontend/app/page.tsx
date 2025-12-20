30df0255fbdcd551a3197c1dcd3e1cea87657b5b
Adiba Tahsin <adiba0tahsin@gmail.com>
2025-12-20 22:16:17 +0600

    feat: add Card, Separator, and Tabs components;
    - implemented card layout, separator, and tab navigation in homepage for testing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,17 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Checkbox } from "@/components/ui/checkbox"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Home() {
   return (
@@ -15,7 +26,30 @@ export default function Home() {
       <Input type="email" placeholder="Email" />
       <Label htmlFor="email">Your email address</Label>
       <Checkbox />
+      <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+    <CardAction>Card Action</CardAction>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+      <Separator />
+      <Tabs defaultValue="account" className="w-[400px]">
+  <TabsList>
+    <TabsTrigger value="account">Account</TabsTrigger>
+    <TabsTrigger value="password">Password</TabsTrigger>
+  </TabsList>
+  <TabsContent value="account">Make changes to your account here.</TabsContent>
+  <TabsContent value="password">Change your password here.</TabsContent>
+</Tabs>
     </div>
+    
   );
 }
 
