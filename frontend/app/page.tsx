42a8dfb25eda5e2682efaf9960c0620645577af8
Adiba Tahsin <adiba0tahsin@gmail.com>
2025-12-20 20:16:10 +0600

    feat: Integrated shadcn/ui and changed color scheme
    - Added shadcn/ui button component with custom variants
    - Updated global color scheme for primary, success, and destructive colors

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
       Hello
+      <Button variant="secondary">Click Me</Button>
+      <Button variant="medical">Medical</Button>
+      <Button variant="transaction">Transaction</Button>
+      <Button variant="emergency">Emergency</Button>
     </div>
   );
 }
