2c7dc7d788fed14edfcfe0b5189b8c221e81c99b
Adi-Sin101 <adiba0tahsin@gmail.com>
2026-04-25 05:46:31 +0600

    fix: fix multiple heads issues in the migration of the medora
    - This commit addresses the multiple heads issues that arose during the migration of the Medora project. The following files were edited to resolve these conflicts:

---
+++ backend/alembic/versions/22e4f4f87d3b_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge heads
+
+Revision ID: 22e4f4f87d3b
+Revises: appt_rev_001, rvw_002
+Create Date: 2026-04-25 05:46:09.704550
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '22e4f4f87d3b'
+down_revision: Union[str, Sequence[str], None] = ('appt_rev_001', 'rvw_002')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
